417e8770ffcf52642d0cf614c0eb1f40caddb356
kobra eslami <kobraeslami.01@gmail.com>
2024-12-19 21:58:43 +0330

    worked on sticky navigation

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,27 +7,46 @@ btnNav.addEventListener('click', function () {
 // smooth scrolling animation
 const allLinks = document.querySelectorAll('a:link');
 allLinks.forEach(function (link) {
-    link.addEventListener('click',function (e){
+    link.addEventListener('click', function (e) {
         e.preventDefault();
         const href = link.getAttribute('href');
 
         //scroll back to top
-        if (href === "#"){
+        if (href === "#") {
             window.scrollTo({
-                top:0,
-                behavior:'smooth'
+                top: 0,
+                behavior: 'smooth'
             })
         }
         // scroll to other links
-        if (href !== "#" && href.startsWith('#')){
-           const section=  document.querySelector(href);
-           section.scrollIntoView({behavior:'smooth'})
+        if (href !== "#" && href.startsWith('#')) {
+            const section = document.querySelector(href);
+            section.scrollIntoView({behavior: 'smooth'})
         }
 
         // close mobile navigation
-        if (link.classList.contains('main-nav-link')){
+        if (link.classList.contains('main-nav-link')) {
             header.classList.toggle('nav-open');
         }
     })
 
 })
+
+// sticky navigation
+const section_hero = document.querySelector('.section-hero');
+const obs = new IntersectionObserver(function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false){
+        document.body.classList.add('sticky');
+    }
+    if (ent.isIntersecting === true){
+        document.body.classList.remove('sticky');
+    }
+
+}, {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin:'-80px'
+})
+obs.observe(section_hero)
